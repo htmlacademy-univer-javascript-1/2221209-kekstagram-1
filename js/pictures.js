63c71f9abf93d11bd71a49aa8b01466cb6bd8b16
GlebOgl <glebogloblin07@gmail.com>
2022-12-17 18:36:00 +0500

    Update pictures.js

--- js/pictures.js
+++ js/pictures.js
@@ -5,17 +5,17 @@ const pictures = document.querySelector('.pictures');
 const picturesFragment = document.createDocumentFragment();
 
 function renderPicture({ url, likes, comments }) {
-    const pictureElement = pictureTemplate.cloneNode(true);
-    pictureElement.querySelector('img').src = url;
-    pictureElement.querySelector('.picture__likes').textContent = likes;
-    pictureElement.querySelector('.picture__comments').textContent = comments.length;
-    return pictureElement;
+  const pictureElement = pictureTemplate.cloneNode(true);
+  pictureElement.querySelector('img').src = url;
+  pictureElement.querySelector('.picture__likes').textContent = likes;
+  pictureElement.querySelector('.picture__comments').textContent = comments.length;
+  return pictureElement;
 }
 
 const generatedPictures = createPhotoObjects();
 generatedPictures.forEach((picture) => {
-    const renderedPicture = renderPicture(picture);
-    picturesFragment.appendChild(renderedPicture);
+  const renderedPicture = renderPicture(picture);
+  picturesFragment.appendChild(renderedPicture);
 });
 
 pictures.appendChild(picturesFragment);
